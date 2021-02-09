601e9c25eaf9bdef2466a34ec9d6bde03617b88c
Goshiq <vangog-2005@ngs.ru>
2021-02-09 12:24:31 +0300

    empty cd done

--- srcs/operations2.c
+++ srcs/operations2.c
@@ -6,34 +6,47 @@
 /*   By: jmogo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/07 14:09:38 by jmogo             #+#    #+#             */
-/*   Updated: 2021/02/08 16:20:45 by jmogo            ###   ########.fr       */
+/*   Updated: 2021/02/09 12:07:23 by jmogo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	check_path(t_all *all)
+void	show_cd_err(t_all *all)
+{
+	ft_putstr_fd("cd: ", STDERR_FILENO);
+	ft_putstr_fd(all->args.args->content, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(strerror(errno), STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+}
+
+void	cd_set_home(t_all *all)
+{
+	char	*tmp;
+
+	tmp = extract_env(all->env, "HOME");
+	if (0 > (chdir(tmp)))
+		show_cd_err(all);
+	free(tmp);
+}
+
+int		check_path(t_all *all)
 {
 	if (all->args.args && all->args.args->content)
+	{
 		if (*((char *)all->args.args->content))
 			return (1);
+	}
+	else
+		cd_set_home(all);
 	return (0);
 }
 
-int	ms_cd(t_all *all)
+int		ms_cd(t_all *all)
 {
-	printf("%p\n", all->args.args);
-	if (all->args.args)
-		printf("%s\n", (char *)all->args.args->content);
 	if (check_path(all))
-//	if (all->args.args->content && *((char *)all->args.args->content)) {
 		if (0 > (chdir(all->args.args->content)))
-		{
-			ft_putstr_fd("cd: ", STDERR_FILENO);
-			ft_putstr_fd(all->args.args->content, STDERR_FILENO);
-			ft_putstr_fd(": ", STDERR_FILENO);
-			ft_putstr_fd(strerror(errno), STDERR_FILENO);
-			ft_putstr_fd("\n", STDERR_FILENO);
-		}
+			show_cd_err(all);
 	return (0);
 }

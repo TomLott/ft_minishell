405427c937cc257ceb860f40bca874acc6ce96ef
Jerald Mogo <jmogo@ev-f3.kzn.21-school.ru>
2021-02-22 18:25:40 +0300

    kek

--- srcs/do_pipe.c
+++ srcs/do_pipe.c
@@ -6,7 +6,7 @@
 /*   By: jmogo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:59:19 by jmogo             #+#    #+#             */
-/*   Updated: 2021/02/22 17:21:02 by jmogo            ###   ########.fr       */
+/*   Updated: 2021/02/22 18:25:30 by jmogo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,10 @@ void	fill_pipes(t_all *all, t_pipi *pipi)
 	while (i++ < all->pipe)
 	{
 		pipe(pp);
-		dup2(pipi->fd1, pp[0]);
-		dup2(pipi->next->fd0, pp[1]);
+		if (pipi->fd1 == 1)
+			dup2(pipi->fd1, pp[1]);
+		if (!pipi->fd0)
+			dup2(pipi->next->fd0, pp[0]);
 		close(pp[0]);
 		close(pp[1]);
 		pipi = pipi->next;
@@ -45,22 +47,38 @@ void	do_pipe(t_all *all, t_pipi *pipi)
 {
 	int	i;
 	int	f;
+	t_pipi *temp;
 
 	i = 0;
 	fill_pipes(all, pipi);
-	while (i < all->pipe)
+	temp = pipi;
+	while (i++ < all->pipe)
 	{
 		f = fork();
+		dup2(pipi->fd1, 1);
+		dup2(pipi->fd0, 0);
 		if (!f)
 		{
-			dup2(pipi->fd1, 1);
+			//if (temp)
+			//	close(temp->fd1);//dup2(pipi->fd1, 1);
 			pipi->args = arr_append(pipi->args, pipi->cmd);
 			if (execve(ft_strjoin("/bin/", pipi->cmd), pipi->args, all->env) < 0)
 				printf(">%s<\terrr 0\n%s\n", pipi->cmd, strerror(errno));
+			//close(pipi->fd0);
 		}
 		else
 			pipi = pipi->next;
 	}
+	i = 0;
+	pipi = temp;
+	while (i++ < all->pipe)
+	{
+		close(temp->fd1);
+		close(temp->fd0);
+		temp = temp->next;
+	}
+	dup2(all->fd1_def, 1);
+	dup2(all->fd0_def, 0);
 	/*
 	printf("pipes = %d\n", all->pipe);
 	pipe(pp);
